c48f44fc296051bf9e06a9b93692bd1283b8396f
ErrroTwo <80226295+ErrorThree@users.noreply.github.com>
2023-03-31 14:32:50 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Get the form and result elements
+const form = document.getElementById('triangular-form');
+const resultDiv = document.getElementById('result');
+
+// Add event listener to the form submit button
+form.addEventListener('submit', function(event) {
+  // Prevent default form submission
+  event.preventDefault();
+  
+  // Get the base and height values from the form
+  const base = parseFloat(document.getElementById('base').value);
+  const height = parseFloat(document.getElementById('height').value);
+  const units = document.getElementById('units').value;
+  // Calculate the area of the triangle
+  const area = 1/3*(base * height);
+  // Display the result
+  resultDiv.innerHTML = `The area of the triangle is ${area.toFixed(2)} ${units}<sup>2</sup>.`;
+});
