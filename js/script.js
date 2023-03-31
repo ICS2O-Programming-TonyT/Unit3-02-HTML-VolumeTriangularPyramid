d947957f4bd80944a826191a98b2650b1d29d18e
ErrroTwo <80226295+ErrorThree@users.noreply.github.com>
2023-03-31 14:33:49 +0000

    e

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ form.addEventListener('submit', function(event) {
   // Prevent default form submission
   event.preventDefault();
   
-  // Get the base and height values from the form
+  // Get the base and height and units values from the form
   const base = parseFloat(document.getElementById('base').value);
   const height = parseFloat(document.getElementById('height').value);
   const units = document.getElementById('units').value;
-  // Calculate the area of the triangle
+  // Calculate the Volume Triangular Pyramid 
   const area = 1/3*(base * height);
   // Display the result
   resultDiv.innerHTML = `The area of the triangle is ${area.toFixed(2)} ${units}<sup>2</sup>.`;
